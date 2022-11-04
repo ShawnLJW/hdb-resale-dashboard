31a962c4091e5969106d162454002fd516234d5e
Shawn Lee Jia Wei <thisisshawnlee@gmail.com>
2022-11-04 20:35:17 +0800

    further cleaning

--- cleaning.R
+++ cleaning.R
@@ -8,3 +8,14 @@ data <- list.files(path="data/prices", full.names = TRUE) %>%
 data[[3]] <- select(data[[3]], -remaining_lease)
 data[[4]] <- select(data[[4]], -remaining_lease)
 data <- bind_rows(data)
+
+# convert values to uppercase
+data <- data %>%
+  mutate(
+    town = replace(town, town == "KALLANG/WHAMPOA", "KALLANG"),
+    flat_type = replace(flat_type, flat_type == "MULTI GENERATION", "MULTI-GENERATION"),
+    flat_model = toupper(flat_model)
+  )
+
+# export file to csv
+write_csv(data, "data/prices_all.csv")
